f588bdb6a2e8675535a98a468500915ce56bbfbc
saipkri <saiprkri@cisco.com>
2017-03-14 10:40:35 +0530

    experimental version of snapshot collection

--- src/main/java/com/sai/pumpkin/domain/GitLogResponse.java
+++ src/main/java/com/sai/pumpkin/domain/GitLogResponse.java
@@ -20,4 +20,17 @@ public class GitLogResponse implements Serializable {
     @Transient
     private List<GitLogEntry> gitLogEntries = new ArrayList<>();
     private List<String> gitLogUUIDs = new ArrayList<>();
+
+    public List<GitLogEntry> getGitLogEntries() {
+        if (gitLogEntries == null) {
+            return new ArrayList<>();
+        } else {
+            return gitLogEntries;
+        }
+    }
+
+    public List<String> getGitLogUUIDs() {
+        return gitLogUUIDs == null ? new ArrayList<>() : gitLogUUIDs;
+    }
+
 }
