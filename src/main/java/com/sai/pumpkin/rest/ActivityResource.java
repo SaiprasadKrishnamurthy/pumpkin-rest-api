3ab98e016b64f89624f7322281b7c3be550d985a
saipkri <saiprkri@cisco.com>
2017-03-16 16:41:23 +0530

    experimental version of snapshot collection

--- src/main/java/com/sai/pumpkin/rest/ActivityResource.java
+++ src/main/java/com/sai/pumpkin/rest/ActivityResource.java
@@ -41,18 +41,4 @@ public class ActivityResource {
         results.forEach(m -> res.put(m.get("_id").toString(), Long.parseLong(m.get("count").toString())));
         return res;
     }
-
-    @ApiOperation("Finds all activities (based on number of commits) since the specified timestamp.")
-    @CrossOrigin(methods = {RequestMethod.POST, RequestMethod.PUT, RequestMethod.OPTIONS, RequestMethod.GET})
-    @RequestMapping(value = "/activity", method = RequestMethod.GET, produces = "application/json")
-    public Map<String, Long> committerActivity(@RequestParam("sinceTimestamp") long sinceTimestamp) {
-        MatchOperation matchStage = Aggregation.match(new Criteria("timestamp").gte(sinceTimestamp));
-        GroupOperation groupOperation = group("artifactConfig.name").count().as("count");
-        Aggregation aggregation = newAggregation(
-                matchStage, groupOperation);
-        AggregationResults<Map> results = mongoTemplate.aggregate(aggregation, MavenGitVersionMapping.class, Map.class);
-        Map<String, Long> res = new TreeMap<>();
-        results.forEach(m -> res.put(m.get("_id").toString(), Long.parseLong(m.get("count").toString())));
-        return res;
-    }
 }
