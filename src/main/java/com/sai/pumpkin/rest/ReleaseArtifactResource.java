dd938321cd89561cfaa030dcacab8b959a858d02
saipkri <saiprkri@cisco.com>
2017-03-17 23:18:37 +0530

    experimental version of snapshot collection

--- src/main/java/com/sai/pumpkin/rest/ReleaseArtifactResource.java
+++ src/main/java/com/sai/pumpkin/rest/ReleaseArtifactResource.java
@@ -5,6 +5,8 @@ import com.sai.pumpkin.domain.ReleaseArtifact;
 import com.sai.pumpkin.repository.MavenGitVersionMappingRepository;
 import com.sai.pumpkin.repository.ReleaseArtifactRepository;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -13,7 +15,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by saipkri on 07/03/17.
@@ -24,6 +28,7 @@ public class ReleaseArtifactResource {
     private final ReleaseArtifactRepository releaseArtifactRepository;
     private final MavenGitVersionMappingRepository mavenGitVersionMappingRepository;
     private final MongoTemplate mongoTemplate;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReleaseArtifactResource.class);
 
     @Inject
     public ReleaseArtifactResource(final ReleaseArtifactRepository releaseArtifactRepository, final MavenGitVersionMappingRepository mavenGitVersionMappingRepository, final MongoTemplate mongoTemplate) {
@@ -40,6 +45,17 @@ public class ReleaseArtifactResource {
         mongoTemplate.remove(Query.query(c), ReleaseArtifact.class);
         mongoTemplate.save(releaseArtifact);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @ApiOperation("Saves a release artifact from a dump")
+    @CrossOrigin(methods = {RequestMethod.POST, RequestMethod.PUT, RequestMethod.OPTIONS, RequestMethod.GET})
+    @RequestMapping(value = "/release", method = RequestMethod.PUT, produces = "application/json", consumes = "text/plain")
+    public ResponseEntity<?> saveReleaseFromDump(@RequestParam("version") String version, @RequestBody String releaseDump) {
+        LOGGER.info("Saving the release dump for: {}", version);
+        LOGGER.info("Dump is \n {} \n\n", releaseDump);
+        Map<String, String> json = new HashMap<>();
+        json.put("status", "success");
+        return new ResponseEntity<>(json, HttpStatus.CREATED);
     }
 
     @ApiOperation("Lists all releases")
