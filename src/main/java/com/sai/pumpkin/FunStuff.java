a6bbf74d39e072743a7c72a72d3d1d5c43802659
prabvara <trojan@dodgyworld.com>
2017-03-26 16:19:16 +0530

    release notification to messaging webhooks based apps

--- src/main/java/com/sai/pumpkin/FunStuff.java
+++ src/main/java/com/sai/pumpkin/FunStuff.java
@@ -1,18 +1,32 @@
 package com.sai.pumpkin;
 
-import java.awt.*;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * Created by saipkri on 21/03/17.
  */
 public class FunStuff {
 
-    public static void mains(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-        while (true) {
 
-            Toolkit.getDefaultToolkit().beep();
-        }
+        long l = 1490434174000L;
+        TimeZone tz = TimeZone.getDefault();
+        int offsetGmtToPst = tz.getOffset(Calendar.ZONE_OFFSET);
+        long adjustedTime = l - offsetGmtToPst;
+        System.out.println(new Date(adjustedTime));
+        System.out.println(adjustedTime);
+
+
+
+
+
 
 
     }
